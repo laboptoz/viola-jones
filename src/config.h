95a9ff05a721e74fddc86fbe32e420cc022d0b9f
guillaume <guillaum324bis@hotmail.fr>
2016-07-30 01:58:55 +0200

    extend configurations

--- src/config.h
+++ src/config.h
@@ -2,8 +2,9 @@
 #define CONFIG_H_INCLUDED
 
 #define INIT_SIZE 24
-#define MIN_IMAGE 43
-#define VARIANCE_THREHOLD 3.9
+#define MIN_IMAGE_TRUE 22
+#define MIN_IMAGE_FALSE 43
+#define VARIANCE_THREHOLD 3.1 //1 - 4.2 // 2 - 5.4 // 3 - 2. // 4 - 5.3 // 5 - 3.5 //
 #define VARIANCE_ERROR 1.
 
 #endif // CONFIG_H_INCLUDED
